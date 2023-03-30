e95a115f7cd35eaada9fb8e50c6c48ea2ee7bb50
akabikas <akabikaskc@gmail.com>
2023-03-30 20:41:05 +0100

    Test cases Created for Order Page

---
+++ src/views/OrderPage.test.js
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import OrderPage from './OrderPage';
+
+describe('OrderPage', () => {
+  test('renders first name field', () => {
+    const { getByLabelText } = render(<OrderPage />);
+    const firstNameInput = getByLabelText('First Name');
+    expect(firstNameInput).toBeInTheDocument();
+  });
+
+  test('renders last name field', () => {
+    const { getByLabelText } = render(<OrderPage />);
+    const lastNameInput = getByLabelText('Last Name');
+    expect(lastNameInput).toBeInTheDocument();
+  });
+
+  test('renders email field', () => {
+    const { getByLabelText } = render(<OrderPage />);
+    const emailInput = getByLabelText('Email');
+    expect(emailInput).toBeInTheDocument();
+  });
+
+  test('renders Address field', () => {
+    const { getByLabelText } = render(<OrderPage />);
+    const firstNameInput = getByLabelText('Address');
+    expect(firstNameInput).toBeInTheDocument();
+  });
+
+  test('renders ingredients checkbox field', () => {
+    const { getByLabelText } = render(<OrderPage />);
+    const ingredientsCheckbox = getByLabelText('Ingredients');
+    expect(ingredientsCheckbox).toBeInTheDocument();
+  });
+
+  test('renders order now button', () => {
+    const { getByText } = render(<OrderPage />);
+    const orderNowButton = getByText('Order Now');
+    expect(orderNowButton).toBeInTheDocument();
+    expect(orderNowButton.tagName).toBe('BUTTON');
+  });
+});
