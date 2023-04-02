b31a64cb75a408888d7376ec5a854ebd3ac6b31b
FrancescStudent <21398313@student.uwl.ac.uk>
2023-04-02 15:05:28 +0100

    pizzaorder

--- src/views/OrderPage.js
+++ src/views/OrderPage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+ 
 
 export default function OrderPage() {
+  const hStyle = { color: 'white'};
   return (
     <>
+    <div>
+      <h1 style={hStyle}>Order Page</h1>
+      <h2 style={hStyle}>Please input your details and ingredients 
+      you would like on your Pizza!</h2>
+      
+    </div>
     
+
+    <Details/>
     </>
+    
   )
+  
 }
