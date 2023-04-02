fae9eeab5edba66b83e679c222e8e854f332de47
akabikas <akabikaskc@gmail.com>
2023-04-02 17:35:31 +0100

    Bootstrap Initialised

--- src/views/OrderPage.js
+++ src/views/OrderPage.js
@@ -28,47 +28,49 @@ function Form() {
 
   return (
     <>
-    <h1>Order Page</h1>
-    <form onSubmit={handleSubmit}>
-      <label>
-        First Name
-        <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Last Name
-        <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Address
-        <input type="text" value={address} onChange={e => setAddress(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Email
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Ingredients
-        <br />
-        <input type="checkbox" value="Cheese" checked={ingredients.includes('Cheese')} onChange={handleCheckboxChange} />
-        <span>Cheese</span>
-        <br />
-        <input type="checkbox" value="Tomatoes" checked={ingredients.includes('Tomatoes')} onChange={handleCheckboxChange} />
-        <span>Tomatoes</span>
-        <br />
-        <input type="checkbox" value="Peppers" checked={ingredients.includes('Peppers')} onChange={handleCheckboxChange} />
-        <span>Peppers</span>
-        <br />
-        <input type="checkbox" value="Mush
+      <div className='container order-page'>
+        <h1>Order Page</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            First Name
+            <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Last Name
+            <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Address
+            <input type="text" value={address} onChange={e => setAddress(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Email
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Ingredients
+            <br />
+            <input type="checkbox" value="Cheese" checked={ingredients.includes('Cheese')} onChange={handleCheckboxChange} />
+            <span>Cheese</span>
+            <br />
+            <input type="checkbox" value="Tomatoes" checked={ingredients.includes('Tomatoes')} onChange={handleCheckboxChange} />
+            <span>Tomatoes</span>
+            <br />
+            <input type="checkbox" value="Peppers" checked={ingredients.includes('Peppers')} onChange={handleCheckboxChange} />
+            <span>Peppers</span>
+            <br />
+            <input type="checkbox" value="Mush
         rooms" checked={ingredients.includes('Mushrooms')} onChange={handleCheckboxChange} />
-        <span>Mushrooms</span>
-      </label>
-      <br />
-      <button type="submit">Order Now</button>
-    </form>
+            <span>Mushrooms</span>
+          </label>
+          <br />
+          <button type="submit">Order Now</button>
+        </form>
+      </div>
     </>
   );
 }
