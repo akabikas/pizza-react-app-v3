bebf2fc71ca6d4377811dc376c6c614516c50b8d
akabikas <akabikaskc@gmail.com>
2023-04-02 18:08:15 +0100

    Font family installed and basic styling for Order Page Done

--- src/views/OrderPage.js
+++ src/views/OrderPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "../assets/css/OrderPage.css";
-
+import logo from '../assets/img/pizza.png';
 function Form() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -30,46 +30,62 @@ function Form() {
     <>
       <div className='container order-page'>
         <h1>Order Page</h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-            First Name
-            <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Last Name
-            <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Address
-            <input type="text" value={address} onChange={e => setAddress(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Email
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Ingredients
+        <div className='order-form col-6'>
+
+
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <label>
+                First Name
+                <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label>
+                Last Name
+                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label>
+                Address
+                <input type="text" value={address} onChange={e => setAddress(e.target.value)} />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label>
+                Email
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label className='ingredients'>
+                Ingredients
+                <div className='list'>
+                  <input className='checkbox' type="checkbox" value="Cheese" checked={ingredients.includes('Cheese')} onChange={handleCheckboxChange} />
+                  <span>Cheese</span>
+                </div>
+                <div className='list'>
+                  <input className='checkbox' type="checkbox" value="Tomatoes" checked={ingredients.includes('Tomatoes')} onChange={handleCheckboxChange} />
+                  <span>Tomatoes</span>
+                </div>
+                <div className='list'>
+                  <input className='checkbox' type="checkbox" value="Peppers" checked={ingredients.includes('Peppers')} onChange={handleCheckboxChange} />
+                  <span>Peppers</span>
+                </div>
+                <div className='list'>
+                  <input className='checkbox' type="checkbox" value="Mushrooms" checked={ingredients.includes('Mushrooms')} onChange={handleCheckboxChange} />
+                  <span>Mushrooms</span>
+                </div>
+              </label>
+            </fieldset>
             <br />
-            <input type="checkbox" value="Cheese" checked={ingredients.includes('Cheese')} onChange={handleCheckboxChange} />
-            <span>Cheese</span>
-            <br />
-            <input type="checkbox" value="Tomatoes" checked={ingredients.includes('Tomatoes')} onChange={handleCheckboxChange} />
-            <span>Tomatoes</span>
-            <br />
-            <input type="checkbox" value="Peppers" checked={ingredients.includes('Peppers')} onChange={handleCheckboxChange} />
-            <span>Peppers</span>
-            <br />
-            <input type="checkbox" value="Mush
-        rooms" checked={ingredients.includes('Mushrooms')} onChange={handleCheckboxChange} />
-            <span>Mushrooms</span>
-          </label>
-          <br />
-          <button type="submit">Order Now</button>
-        </form>
+            <button type="submit" className='OrderNowButton'>Order Now</button>
+          </form>
+        </div>
+        <div className='pizza-image col-6'>
+          <img src={logo} ></img>
+        </div>
       </div>
     </>
   );
