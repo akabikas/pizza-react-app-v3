221f79f8eeda7deef3e0016c10146f87e3b02494
akabikas <akabikaskc@gmail.com>
2023-04-06 11:40:02 +0100

    Routing Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import OrderPage from "./views/OrderPage";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <OrderPage />
-
+      <Router>
+            <Routes>
+              <Route end path="/" element={<OrderPage />} />
+              <Route path="/favorites" element={"favorites page to be created"} />
+          </Routes>
+      </Router>
     </>
   );
 }
