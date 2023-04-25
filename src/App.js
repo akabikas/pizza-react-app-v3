ed9b50e5645dbd7ef2c41f3d3fb5e961b560ad1c
akabikas <akabikaskc@gmail.com>
2023-04-25 21:18:32 +0100

    Login page added, Routes Updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PizzaTable from "./views/Pizzatable";
 import Header from "./components/Header";
 import HomePage from "./views/HomePage";
+import Login from "./views/Login";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route end path="/" element={<HomePage />} />
           <Route end path="/order" element={<OrderPage />} />
           <Route path="/favorites" element={<PizzaTable />} />
+          <Route path="/login" element={<Login />} />
+
         </Routes>
       </Router>
 
