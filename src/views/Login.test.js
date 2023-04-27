aee88b53d850c0d94c94dacdde43033a779cac97
akabikas <akabikaskc@gmail.com>
2023-04-27 11:27:17 +0100

    Login and Register screen done

--- src/views/Login.test.js
+++ src/views/Login.test.js
@@ -1,33 +1,36 @@
-import { render } from "@testing-library/react";
-import OrderPage from "./OrderPage";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Login from "./Login";
 
-describe("OrderPage", () => {
+
+describe("Login", () => {
   test("renders first name field", () => {
-    const { getByLabelText } = render(<OrderPage />);
+    const { getByLabelText } = render(<Login />);
     const firstNameInput = getByLabelText("First Name");
     expect(firstNameInput).toBeInTheDocument();
   });
 
   test("renders last name field", () => {
-    const { getByLabelText } = render(<OrderPage />);
+    const { getByLabelText } = render(<Login />);
     const lastNameInput = getByLabelText("Last Name");
     expect(lastNameInput).toBeInTheDocument();
   });
 
   test("renders email field", () => {
-    const { getByLabelText } = render(<OrderPage />);
+    const { getByLabelText } = render(<Login />);
     const emailInput = getByLabelText("Email");
     expect(emailInput).toBeInTheDocument();
   });
 
   test("renders password field", () => {
-    const { getByLabelText } = render(<OrderPage />);
+    const { getByLabelText } = render(<Login />);
     const passwordInput = getByLabelText("Password");
     expect(passwordInput).toBeInTheDocument();
   });
 
   test("renders confirmpassword field", () => {
-    const { getByLabelText } = render(<OrderPage />);
+    const { getByLabelText } = render(<Login />);
     const confirmpasswordInput = getByLabelText("Confirm Password");
     expect(confirmpasswordInput).toBeInTheDocument();
   });
